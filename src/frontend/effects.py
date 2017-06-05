c265482653b5331578254cfe708fca2e74ecd35f
Mário Alexandre Lopes Liberato <gnmpolicemata91@gmail.com>
2017-06-05 01:38:27 +0100

    Added lomography effect

--- src/frontend/effects.py
+++ src/frontend/effects.py
@@ -12,6 +12,23 @@ def effect_image(file_name, effect_name, args=None):
         return -1
 
 
+# Lomography Effect
+def lomography(im, arg=None):
+    nim = Image.new(im.mode, im.size)
+    width, height = im.size
+    for x in range(width):
+        for y in range(height):
+            p = im.getpixel((x, y))
+            r = p[0]
+            g = p[1]
+            b = p[2]
+            nb = int(round(r * 0.189 + g * 0.769 + b * 0.393))
+            ng = int(round(r * 0.168 + g * 0.686 + b * 0.349))
+            nr = int(round(r * 0.131 + g * 0.534 + b * 0.272))
+            nim.putpixel((x, y), (nr, ng, nb))
+    return nim
+
+
 # Sepia Effect
 def sepia(im, arg=None):
     nim = Image.new(im.mode, im.size)
@@ -34,4 +51,4 @@ def sepia(im, arg=None):
 def grayscale(im, arg=None):
     return im.convert("L")
 
-print(effect_image("1.jpg", sepia))
+print(effect_image("1.jpg", lomography))
