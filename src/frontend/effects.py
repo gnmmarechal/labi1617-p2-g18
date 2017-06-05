6cb7486496fda144d8d2f40cc37ca36a044fdc1a
Mário Alexandre Lopes Liberato <gnmpolicemata91@gmail.com>
2017-06-05 01:34:08 +0100

    Added sepia effect

--- src/frontend/effects.py
+++ src/frontend/effects.py
@@ -3,17 +3,35 @@ from misc_module import remove_extension as rem_ext
 
 
 # Modify an image with the selected effect
-def effect_image(file_name, effect_name):
+def effect_image(file_name, effect_name, args=None):
     try:
-        im = effect_name(Image.open(file_name))
+        im = effect_name(Image.open(file_name), args)
         im.save(rem_ext(file_name)+".png")
         return 0
     except Exception:
         return -1
 
 
+# Sepia Effect
+def sepia(im, arg=None):
+    nim = Image.new(im.mode, im.size)
+    width, height = im.size
+
+    for x in range(width):
+        for y in range(height):
+            p = im.getpixel((x,y))
+            r = p[0]
+            g = p[1]
+            b = p[2]
+            nr = int(round(r * 0.189 + g * 0.769 + b * 0.393))
+            ng = int(round(r * 0.168 + g * 0.686 + b * 0.349))
+            nb = int(round(r * 0.131 + g * 0.534 + b * 0.272))
+            nim.putpixel((x, y), (nr, ng, nb))
+    return nim
+
+
 # Grayscale Effect
-def grayscale(im):
+def grayscale(im, arg=None):
     return im.convert("L")
 
-print(effect_image("1.jpg", grayscale))
+print(effect_image("1.jpg", sepia))
