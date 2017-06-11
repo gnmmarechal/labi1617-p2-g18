012730258daed8a41655746916c6bd9b98b87841
Jorge Oliveira <jorge.am.oliveira@ua.pt>
2017-06-11 16:04:30 +0100

    Almost finished meme.py|Updated rel|Reworked index

--- src/webapp/meme.py
+++ src/webapp/meme.py
@@ -1,9 +1,8 @@
 #Coded on Python3
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 from misc_module import remove_extension as rem_ext
-#from face import *
-
-#outline está sempre true
+import cv2
+#OpenCV2 Only used for face recognition
 
 def meme_image(file_name, effect_name, args=None):
     try:
@@ -71,22 +70,34 @@ def black_and_white(im):
 #Apply another background
 #file_name is the background
 #The app should call face.py 1st, and then apply bkgrd
-def addbackground(im):
-	im_w, im_h = im.size
+def addbackground(im,args):
+	#Type of Bg; Circle,Triangles,Blank
+	if len(args) > 1:
+		raise Exception("Wrong argument length")
+        
+	bg_pick = args[0] 
+	#Cut face from image
+	face_im = facecut(im)
 	
-	background = Image.new("RGBA", (640, 480), (255, 255, 255, 255))
+	im_w, im_h = face_im.size	
+	
+	if bg_pick == "circle":
+		background = dobg2()	
+	elif bg_pick == "triangle":
+		background = dobg()	
+	else:	
+		Image.new("RGB", (640, 480), (255, 255, 255))
 	
 	offset = ((640 - img_w)/2, (480 - img_h)/2)
 	background.paste(im, offset)
-	background.save("out.png")
 	return background
 
-#Testing bg
+#Triangle Background
 def dobg():
 	
-	im = Image.new("RGB", (640, 480), (255, 255, 255))
+	bg = Image.new("RGB", (640, 480), (255, 255, 255))
 	
-	draw = ImageDraw.Draw(im)
+	draw = ImageDraw.Draw(bg)
 	
 	draw.polygon([(0,0), (320,0), (320,240)], fill = (255,0,0))
 	draw.polygon([(0,0), (0,330), (320,240)], fill = (255,125,0))
@@ -98,14 +109,15 @@ def dobg():
 	draw.polygon([(320,480), (640,480), (320,240)], fill = (255,0,255))
 	
 	
-	im.save("color.png")
+	return bg
+
+#Circle Background
+def dobg2(args=None):
 	
-def dobg2():
+	bg = Image.new("RGB", (640, 480), (255, 255, 255))
 	
-	im = Image.new("RGB", (640, 480), (255, 255, 255))
-	
-	draw = ImageDraw.Draw(im)
-	#320,240
+	draw = ImageDraw.Draw(bg)
+
 	draw.polygon([(0,0),(0,480),(640,480),(640,0)], fill = (255,0,150))
 	draw.ellipse((-40,-120, 680, 600), fill = (255,0,255), outline ='black')
 	draw.ellipse((-20,-100, 660, 580), fill = (150,0,255), outline ='black')
@@ -123,4 +135,41 @@ def dobg2():
 	draw.ellipse((240,160, 400, 320), fill = 'gray', outline ='black')
 	draw.ellipse((280,200, 360, 280), fill = 'black', outline ='white')
 	
-	im.save("circle.png")
+	return bg
+
+#This function was adapted from different
+#http://gregblogs.com/computer-vision-cropping-faces-from-images-using-opencv2/
+#http://docs.opencv.org/trunk/d7/d8b/tutorial_py_face_detection.html
+#https://realpython.com/blog/python/face-recognition-with-python/
+
+def facecut(file_name):
+	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+	img = cv2.imread(file_name)
+	height = img.shape[0]
+	width = img.shape[1]
+	size = height * width
+
+	if size > (500^2):
+		r = 500.0 / img.shape[1]
+		dim = (500, int(img.shape[0] * r))
+		img2 = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+		img = img2
+	#Convert from Color to Gray
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	eyesn = 0
+
+	for (x,y,w,h) in faces:
+		imgCrop = img[y:y+h,x:x+w]
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = img[y:y+h, x:x+w]
+
+		eyes = eye_cascade.detectMultiScale(roi_gray)
+		for (ex,ey,ew,eh) in eyes:
+			eyesn = eyesn +1
+		if eyesn >= 2:
+			imgCrop = cv2.cvtColor(imgCrop, cv2.COLOR_BGR2RGB)
+			im_pil = Image.fromarray(imgCrop)
+			#im_pil.save("face"+file_name+".png")
+			return im_pil
